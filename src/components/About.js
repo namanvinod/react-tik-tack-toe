3c16c74ba8bcf31a1b56374c80cb0a12683747fe
Naman Vinod <namanvinod1@gmail.com>
2021-09-27 11:48:32 +0530

    Adding About and Profile Components

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+const About = () => {
+    return (
+        <div>This is About Page of Tick Tack Toe</div>
+    );
+};
+
+export default About;
---
+++ src/components/MyProfile.js
@@ -0,0 +1,7 @@
+const MyProfile = () => {
+    return (
+        <div>This is My Profile</div>
+    );
+};
+
+export default MyProfile;
