2ae260b6b2927865242942162aeb9ae043374508
Naman Vinod <namanvinod1@gmail.com>
2021-09-27 11:48:55 +0530

    Adding Routing

--- src/core/components/Navbar.js
+++ src/core/components/Navbar.js
@@ -1,14 +1,25 @@
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import './Navbar.css';
+
+import Game from '../../components/Game';
+import About from '../../components/About';
+import MyProfile from '../../components/MyProfile';
 
 const Navbar = () => {
     return (
-        <div className="nav-container">
-            <ul>Game</ul>
-            <ul>About</ul>
-            <ul className="nav-profile"><a>My Profile</a></ul>
-        </div>
+        <Router>
+            <div className="nav-container">
+                <ul><Link to="/">Game</Link></ul>
+                <ul><Link to="/about">About</Link></ul>
+                <ul className="nav-profile"><Link to="/profile">My Profile</Link></ul>
+            </div>
+            <Route exact path="/" component={Game}></Route>
+            <Route exact path="/about" component={About}></Route>
+            <Route exact path="/profile" component={MyProfile}></Route>
+        </Router>
     );
-}
+};
 
 export default Navbar;
 
