dcce90a3ad8aaaeee3a204055c4fc7759c1514ef
Naman Vinod <namanvinod1@gmail.com>
2021-09-27 11:51:12 +0530

    Refactoring App to accomodate new Routing Changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 
-import Game from './components/Game';
 import Navbar from './core/components/Navbar';
 
 const App = () => {
   return (
     <div className="app-container">
       <Navbar />
-      <Game />
     </div>
   );
 };
