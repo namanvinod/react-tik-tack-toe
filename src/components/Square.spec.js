b9d7b3901c25036bbd86ebf741cebc78302603bc
Naman Vinod <namanvinod1@gmail.com>
2021-09-28 10:04:32 +0530

    Updated Square Unit Tests

--- src/components/Square.spec.js
+++ src/components/Square.spec.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
 
 import Square from './Square';
 
-describe('Square', () => {
+describe('Square Load', () => {
     let container;
-    beforeEach(() => (container = shallow(<Square squareAction={jest.fn()} squareValue={''}/>)));
+    beforeEach(() => (container = shallow(<Square squareAction={jest.fn()} squareValue={'O'} squareIndex={0}/>)));
 
     it('should render Square', () => {
-		expect(container.find('button').length).toEqual(1);
+		  expect(container.find('button').length).toEqual(1);
     });
+
+    it('should render given value', () => {
+      expect(container.find('button').text()).toEqual('O');
+    });
+});
+
+describe('Square Click', () => {
+  let container, spy;
+
+  beforeEach(() => {
+    spy = sinon.spy();
+    container = mount(<Square squareValue={'O'} squareIndex={101} squareAction={spy}/>);
+  });
+
+  it('Click Event of Square is fired when it is clicked from square', () => {
+    expect(spy.notCalled).toBeTruthy();
+    container.find('.square').first().simulate('click');
+    expect(spy.calledOnce).toBeTruthy();
+  });
+
+  it('Click Event of Square is fired with correct param when it is clicked from square', () => {
+    expect(spy.notCalled).toBeTruthy();
+    container.find('.square').first().simulate('click');
+    expect(spy.calledWith(101));
+  });
 });
