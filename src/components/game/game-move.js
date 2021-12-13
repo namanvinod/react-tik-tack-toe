21ffbcf1b0ef5e6bfa29b0b3dee05cc83514d8bd
Naman Vinod <namanvinod1@gmail.com>
2021-12-13 15:05:27 +0530

    Compilation Error resolution

--- src/components/game/game-move.js
+++ src/components/game/game-move.js
@@ -1,7 +1,7 @@
 const calculateSquare = (index) => `(${parseInt((index/3), 0) + 1},${(index%3) + 1})`;
 
 const GameMove = ({ moveId, moveNumber, squareIndex, squareValue, undoMove, canUndo }) => (
-    <li key={id} className="current-game-move">
+    <li key={moveId} className="current-game-move">
         {`Move ${moveNumber}: ${squareValue} played at ${calculateSquare(squareIndex)}.`}
         <button disabled={!canUndo} onClick={() => undoMove(squareIndex)}>Make This Current Move</button>
     </li>
