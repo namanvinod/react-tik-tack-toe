6548b69cf8fbdb1d3feb5d14928e5f6f0b234fd9
Naman Vinod <namanvinod1@gmail.com>
2021-12-27 20:17:44 +0530

    Using Popup component

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
+
 import App from './App';
+import Popup from './core/components/pop-up';
+
 import { gameStore } from './store/gameStore';
 
 ReactDOM.render(
   <Provider store={gameStore}>
     <App />
+    <Popup show>
+    </Popup>
   </Provider>,
   document.getElementById('root')
 );
