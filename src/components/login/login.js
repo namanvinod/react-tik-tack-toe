2d0865b1417ccd7250c37df9d40a87950a4f9206
Naman Vinod <namanvinod1@gmail.com>
2021-12-28 13:24:27 +0530

    Updated Login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,14 +1,33 @@
-export default login = () => {
+import './login.css';
+
+import { useRef } from 'react';
+
+export default () => {
+    const userNameRef = useRef('');
+    const passwordRef = useRef('');
+
+    const handleLogin = () => {
+        console.log(userNameRef.current, passwordRef);
+    }
+
+    const handleRegister = () => {
+        console.log(userNameRef.current.value, passwordRef);
+    }
+
     return (
         <div className="login-container">
-            <label for="userName">User Name</label>
-            <input type="text" />
-            <label for="password">Password</label>
-            <input type="text" />
-            <button>
+            <div className='login-section'>
+                <label htmlFor='userName'>User Name</label>
+                <input type="text" ref={userNameRef} />
+            </div>
+            <div className='login-section'>
+                <label htmlFor='password'>Password</label>
+                <input type="password" ref={passwordRef} />
+            </div>
+            <button onClick={handleLogin}>
                 Login
             </button>
-            <button>
+            <button onClick={handleRegister}>
                 Register
             </button>
 
--- src/index.js
+++ src/index.js
@@ -8,13 +8,17 @@ import './index.css';
 
 import App from './App';
 import Popup from './core/components/pop-up';
+import Login from './components/login/login';
 
 import { gameStore } from './store/gameStore';
 
 ReactDOM.render(
   <Provider store={gameStore}>
     <App />
-    <Popup show>
+    <Popup 
+      show={true}
+    >
+      <Login />
     </Popup>
   </Provider>,
   document.getElementById('root')
