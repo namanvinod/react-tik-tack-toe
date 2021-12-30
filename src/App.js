2cf27b86d0f29d79e2834d5d9e303a6263b3c99f
Naman Vinod <namanvinod1@gmail.com>
2021-12-30 17:25:27 +0530

    Sending Login again as a child to reset ref

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
   return (
     <>
       <Popup show={showPopup}>
-        <Login></Login>
+        {showPopup && <Login></Login>}
       </Popup>
       <HashRouter>
         <div className="app-container">
