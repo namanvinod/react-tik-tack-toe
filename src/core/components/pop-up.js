a8a826a247bdcb44147dc05321d6b44c7bdfd2fe
Naman Vinod <namanvinod1@gmail.com>
2021-12-30 19:29:15 +0530

    Refactoring

--- src/core/components/pop-up.js
+++ src/core/components/pop-up.js
@@ -4,19 +4,19 @@ const Popup = ({ handleClose, show, children }) => {
     const showHideClassName = show ? 'modal display-block' : 'modal display-none';
   
     return (
-      <div className={showHideClassName}>
-        <section className="modal-main">
-          <div className='float-right'>
-            <i className='fas fa-window-close' onClick={handleClose}></i>
-          </div>
-          <div className='error-container'>
-            Error: 
-          </div>            
-          <div className='display-inline'>
-            {children}
-          </div>
-        </section>
-      </div>
+        <div className={showHideClassName}>
+            <section className="modal-main">
+                <div className='float-right'>
+                    <i className='fas fa-window-close' onClick={handleClose}></i>
+                </div>
+                <div className='error-container'>
+                    Error:
+                </div>
+                <div className='display-inline'>
+                    {children}
+                </div>
+            </section>
+        </div>
     );
 };
 
