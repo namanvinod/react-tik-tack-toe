c79fe5cbf8cb12a0427ffb9f4c993a7a0c940761
Naman Vinod <namanvinod1@gmail.com>
2022-03-12 22:30:42 +0530

    Updated Square Tests

--- src/components/square/Square.spec.js
+++ src/components/square/Square.spec.js
@@ -4,9 +4,16 @@ import sinon from 'sinon';
 
 import Square from './Square';
 
+import { Provider } from 'react-redux';
+import gameReducer from '../../store/gameReducer';
+import { createStore } from 'redux';
+
 describe('Square Load', () => {
     let container;
-    beforeEach(() => (container = shallow(<Square squareAction={jest.fn()} squareValue={'O'} squareIndex={0}/>)));
+    beforeEach(() => {
+      const store = createStore(gameReducer);
+      container = mount(<Provider store={store}><Square squareAction={jest.fn()} squareValue={'O'} squareIndex={0}/></Provider>)
+    });
 
     it('should render Square', () => {
 		  expect(container.find('button').length).toEqual(1);
@@ -22,7 +29,8 @@ describe('Square Click', () => {
 
   beforeEach(() => {
     spy = sinon.spy();
-    container = mount(<Square squareValue={'O'} squareIndex={101} squareAction={spy}/>);
+    const store = createStore(gameReducer);
+    container = mount(<Provider store={store}><Square squareValue={'O'} squareIndex={101} squareAction={spy}/></Provider>);
   });
 
   it('Click Event of Square is fired when it is clicked from square', () => {
