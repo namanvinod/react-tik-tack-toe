51b2de14e9fcddaad483bde93365cade37897f9b
Naman Vinod <namanvinod1@gmail.com>
2022-03-13 22:13:56 +0530

    Updating Game Tests

--- src/components/game/Game.spec.js
+++ src/components/game/Game.spec.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 
 import Game from './Game';
 import Board from '../board/Board';
+import { GAME_STATE, PLAYERS } from '../../core/enum';
 
 import { Provider } from 'react-redux';
 import gameReducer from '../../store/gameReducer';
 import { createStore } from 'redux';
 
+const storeInitState = { 
+  games: [], 
+  currentGame: {
+      moveSet: [],
+      winner: '',
+      gameState: GAME_STATE.NEW,
+      currentPlayer: PLAYERS.X
+  }
+};
+
 describe('Game', () => {
     let container;
-    beforeEach(() => container = mount(<Provider store={createStore(gameReducer)}><Game /></Provider>));
+    beforeEach(() => container = mount(<Provider store={createStore(gameReducer, storeInitState)}><Game /></Provider>));
 
     it('should render Board', () => {
 		  expect(container.containsMatchingElement(<Board />)).toBeTruthy();
@@ -23,7 +34,7 @@ xdescribe('Game Load', () => {
   
   beforeEach(() => {
     const container = mount(<Provider store={createStore(gameReducer)}><Game /></Provider>);
-    gameInfo = container.find('.game-info');
+    gameInfo = container.find('.game-info-container');
   });
 
   it('displays the value of current player correctly when board loads', () => {  
@@ -39,7 +50,7 @@ xdescribe('Game Play', () => {
     firstBoardRow = container.find('.board-row').first();
     firstSquare = firstBoardRow.find('.square').first();
     secondSquare = firstBoardRow.find('.square').at(1);
-    gameInfo = container.find('.game-info');
+    gameInfo = container.find('.game-info-container');
   });
 
   it('updates the value of first square when it is clicked', () => {  
@@ -47,26 +58,26 @@ xdescribe('Game Play', () => {
     expect(firstSquare.text()).toEqual('X');
   });
 
-  it('updates the background of first square when it is clicked', () => {  
+  xit('updates the background of first square when it is clicked', () => {  
     expect(container.find('.board-row').first().find('.square').first().hasClass('first-player')).toBeFalsy();
     firstSquare.simulate('click');
     expect(container.find('.board-row').first().find('.square').first().hasClass('first-player')).toBeTruthy();
     expect(container.find('.board-row').first().find('.square').first().hasClass('second-player')).toBeFalsy();
   });
 
-  it('updates the value of current player when it is clicked', () => {  
+  xit('updates the value of current player when it is clicked', () => {  
     firstSquare.simulate('click');
     expect(gameInfo.text()).toEqual('Current Player: O');
   });
 
-  it('updates the value of first two squares when clicked', () => {  
+  xit('updates the value of first two squares when clicked', () => {  
     firstSquare.simulate('click');
     secondSquare.simulate('click');
     expect(firstSquare.text()).toEqual('X');
     expect(secondSquare.text()).toEqual('O');
   });
 
-  it('updates the background of first two squares when clicked', () => {  
+  xit('updates the background of first two squares when clicked', () => {  
     expect(container.find('.board-row').first().find('.square').some('.first-player')).toBeFalsy();
     expect(container.find('.board-row').first().find('.square').some('.second-player')).toBeFalsy();
     
@@ -80,7 +91,7 @@ xdescribe('Game Play', () => {
     expect(container.find('.board-row').first().find('.square').at(1).hasClass('second-player')).toBeTruthy();
   });
 
-  it('updates the value of current player when two squares are clicked', () => {  
+  xit('updates the value of current player when two squares are clicked', () => {  
     firstSquare.simulate('click');
     secondSquare.simulate('click');
     expect(gameInfo.text()).toEqual('Current Player: X');
