65c748053af27b292b90a17cf70dc4c3152d7e13
Naman Vinod <namanvinod1@gmail.com>
2022-03-14 23:01:23 +0530

    Added classes

--- src/components/action-btn-container/action-btn-container.js
+++ src/components/action-btn-container/action-btn-container.js
@@ -9,8 +9,8 @@ const ActionBtnContainer = ({ createNewGame, resetGame }) => {
     
     return (
         <div className="action-btn-container">
-            <button className="btn btn-outline-primary" onClick={createNewGame} disabled={gameState === GAME_STATE.NEW}>New Game</button>
-            <button className="btn btn-outline-primary" onClick={resetGame} disabled={gameState !== GAME_STATE.IN_PROGRESS}>Reset Current Game</button>
+            <button className="btn btn-outline-primary btn-create" onClick={createNewGame} disabled={gameState === GAME_STATE.NEW}>New Game</button>
+            <button className="btn btn-outline-primary btn-reset" onClick={resetGame} disabled={gameState !== GAME_STATE.IN_PROGRESS}>Reset Current Game</button>
         </div>
     );
 };
