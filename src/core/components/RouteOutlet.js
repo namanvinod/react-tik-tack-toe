65df8d1bdcf2fc196bbaa2f15a15f83c4d8d349c
resourceinfotechsolution <namanvinod1@gmail.com>
2023-02-09 19:20:36 +0530

    adding mock paper functionality

--- src/core/components/RouteOutlet.js
+++ src/core/components/RouteOutlet.js
@@ -1,12 +1,16 @@
-import { Fragment } from 'react';
-import { Route } from 'react-router-dom';
+import { Fragment } from "react";
+import { Route } from "react-router-dom";
 
-import Game from '../../components/game/Game';
-import About from '../../components/about/About';
-import MyProfile from '../../components/my-profile/MyProfile';
+import Game from "../../components/game/Game";
+import About from "../../components/about/About";
+import MyProfile from "../../components/my-profile/MyProfile";
+import { QuestionBankDashboard } from "../..//components/mock-paper/QuestionBankDashboard";
 
-export const RouterOutlet = () =>   <Fragment>
-                                        <Route exact path="/" component={Game}></Route>
-                                        <Route exact path="/about" component={About}></Route>
-                                        <Route exact path="/profile" component={MyProfile}></Route>
-                                    </Fragment>;
+export const RouterOutlet = () => (
+  <Fragment>
+    <Route exact path="/" component={Game}></Route>
+    <Route exact path="/about" component={About}></Route>
+    <Route exact path="/profile" component={MyProfile}></Route>
+    <Route exact path="/mockpaper" component={QuestionBankDashboard}></Route>
+  </Fragment>
+);
