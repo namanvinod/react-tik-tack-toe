d1c451638548e87a71bac1d2a5cc4e01d63b7c23
resourceinfotechsolution <namanvinod1@gmail.com>
2023-02-09 21:38:38 +0530

    added grid

--- src/components/mock-paper/QuestionBankDashboard.js
+++ src/components/mock-paper/QuestionBankDashboard.js
@@ -1,17 +1,55 @@
+import { useState } from "react";
 import { questonBank } from "./questionBank";
+import "./QuestionBankDashboard.css";
 
 export const QuestionBankDashboard = () => {
+  const [selectedQuestions, setSelecteduestions] = useState([]);
+
+  const onQuestionSelect = (question) => {
+    const updatedQuestion = selectedQuestions.find(
+      (q) => q.questionId === question.questionId
+    );
+    if (updatedQuestion) {
+      const indexOfRemovedQuestion = selectedQuestions.findIndex(
+        (ques) => ques !== question.questionId
+      );
+
+      const updatedQuestions = [...selectedQuestions];
+
+      for (
+        let index = indexOfRemovedQuestion + 1;
+        index < selectedQuestions.length;
+        index++
+      ) {
+        updatedQuestions[index].questionNumber =
+          updatedQuestions[index].questionNumber - 1;
+      }
+      setSelecteduestions(
+        updatedQuestions.filter((q) => q.questionId !== question.questionId)
+      );
+    } else {
+      const lastQuestionNumber =
+        selectedQuestions.length > 0
+          ? selectedQuestions[selectedQuestions.length - 1].questionNumber
+          : 0;
+      setSelecteduestions((prev) => [
+        ...prev,
+        { ...question, questionNumber: lastQuestionNumber + 1 },
+      ]);
+    }
+  };
+
   return (
     <>
-      <div className="container">
+      <div className="container margin-y-20">
         <div className="row">
-          <div className="col-md-4">Question</div>
-          <div className="col-md-3">Answers</div>
-          <div className="col-md-3">Exams</div>
-          <div className="col-md-1">Difficulty</div>
-          <div className="col-md-1">Select</div>
+          <div className="col-md-4 grid-header">Question</div>
+          <div className="col-md-3 grid-header">Answers</div>
+          <div className="col-md-3 grid-header">Exams</div>
+          <div className="col-md-1 grid-header">Difficulty</div>
+          <div className="col-md-1 grid-header">Select</div>
         </div>
-        {questonBank.map((quesion, idx) => {
+        {questonBank.map((quesion) => {
           return (
             <div className="row">
               <div className="col-md-4">{quesion.questionText}</div>
@@ -19,12 +57,74 @@ export const QuestionBankDashboard = () => {
               <div className="col-md-3">{quesion.exams.join(", ")}</div>
               <div className="col-md-1">{quesion.difficulty}</div>
               <div className="col-md-1">
-                <checkbox id={idx}></checkbox>
+                <input
+                  type="checkbox"
+                  onChange={() => onQuestionSelect(quesion)}
+                />
               </div>
             </div>
           );
         })}
       </div>
+      {selectedQuestions.length > 0 && (
+        <>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4 grid-header">Question</div>
+              <div className="col-md-2 grid-header">Question Number</div>
+            </div>
+            {selectedQuestions.map((selectedQuestion, idx) => {
+              return (
+                <div className="row width-500">
+                  <div className="col-md-4">
+                    {
+                      questonBank.find(
+                        (ques) =>
+                          ques.questionId === selectedQuestion.questionId
+                      )?.questionText
+                    }
+                  </div>
+                  <div className="col-md-1">
+                    {selectedQuestion.questionNumber}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="sample-question-paper">
+            {selectedQuestions.map((selectedQuestion) => {
+              return (
+                <>
+                  <div className="container margin-y-20">
+                    <div className="row">
+                      <div className="col-md-12 margin-y-5">
+                        Question {`${selectedQuestion.questionNumber}. `}
+                        {selectedQuestion.questionText}
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6">
+                        a. {selectedQuestion.answers[0]}
+                      </div>
+                      <div className="col-md-6">
+                        b. {selectedQuestion.answers[1]}
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6">
+                        c. {selectedQuestion.answers[2]}
+                      </div>
+                      <div className="col-md-6">
+                        d. {selectedQuestion.answers[3]}
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </>
+      )}
     </>
   );
 };
--- src/components/mock-paper/questionBank.js
+++ src/components/mock-paper/questionBank.js
@@ -13,30 +13,35 @@ const EXAMS = {
 
 export const questonBank = [
   {
+    questionId: 0,
     questionText: "Zeroth question?",
     answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
     difficulty: DIFFICULTY.hard,
     exams: [EXAMS.upNEET, EXAMS.upPCS],
   },
   {
+    questionId: 1,
     questionText: "First question?",
     answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
     difficulty: DIFFICULTY.hard,
     exams: [EXAMS.upNEET, EXAMS.upPCS],
   },
   {
+    questionId: 2,
     questionText: "Second question?",
     answers: ["Answer5", "Answer8", "Answer3", "Answer4"],
     difficulty: DIFFICULTY.hard,
     exams: [EXAMS.upNEET, EXAMS.upPCS],
   },
   {
+    questionId: 3,
     questionText: "Third question?",
     answers: ["Answer1", "Answer44", "Answer4", "Answer6"],
     difficulty: DIFFICULTY.hard,
     exams: [EXAMS.upNEET, EXAMS.upPCS],
   },
   {
+    questionId: 4,
     questionText: "Very Very long text in this question?",
     answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
     difficulty: DIFFICULTY.hard,
