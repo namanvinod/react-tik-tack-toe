18eb9590544a20ef13c216f3c9163ba4fbf159f0
namanvinod <51288119+namanvinod@users.noreply.github.com>
2023-02-10 20:38:29 +0530

    Mock paper (#6)
    * added grid
    
    * Adding Download PDF

--- src/components/mock-paper/QuestionBankDashboard.js
+++ src/components/mock-paper/QuestionBankDashboard.js
@@ -1,17 +1,73 @@
+import { useState, useRef } from "react";
 import { questonBank } from "./questionBank";
+import "./QuestionBankDashboard.css";
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
 
 export const QuestionBankDashboard = () => {
+  const [selectedQuestions, setSelecteduestions] = useState([]);
+
+  const printRef = useRef();
+
+  const handleDownloadPdf = async () => {
+    const element = printRef.current;
+    const canvas = await html2canvas(element);
+    const data = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF();
+    const imgProperties = pdf.getImageProperties(data);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, "PNG", 0, 0, pdfWidth, pdfHeight);
+    pdf.save("print.pdf");
+  };
+
+  const onQuestionSelect = (question) => {
+    const updatedQuestion = selectedQuestions.find(
+      (q) => q.questionId === question.questionId
+    );
+    if (updatedQuestion) {
+      const indexOfRemovedQuestion = selectedQuestions.findIndex(
+        (ques) => ques !== question.questionId
+      );
+
+      const updatedQuestions = [...selectedQuestions];
+
+      for (
+        let index = indexOfRemovedQuestion + 1;
+        index < selectedQuestions.length;
+        index++
+      ) {
+        updatedQuestions[index].questionNumber =
+          updatedQuestions[index].questionNumber - 1;
+      }
+      setSelecteduestions(
+        updatedQuestions.filter((q) => q.questionId !== question.questionId)
+      );
+    } else {
+      const lastQuestionNumber =
+        selectedQuestions.length > 0
+          ? selectedQuestions[selectedQuestions.length - 1].questionNumber
+          : 0;
+      setSelecteduestions((prev) => [
+        ...prev,
+        { ...question, questionNumber: lastQuestionNumber + 1 },
+      ]);
+    }
+  };
+
   return (
     <>
-      <div className="container">
+      <div className="container margin-y-20">
         <div className="row">
-          <div className="col-md-4">Question</div>
-          <div className="col-md-3">Answers</div>
-          <div className="col-md-3">Exams</div>
-          <div className="col-md-1">Difficulty</div>
-          <div className="col-md-1">Select</div>
+          <div className="col-md-4 grid-header">Question</div>
+          <div className="col-md-3 grid-header">Answers</div>
+          <div className="col-md-3 grid-header">Exams</div>
+          <div className="col-md-1 grid-header">Difficulty</div>
+          <div className="col-md-1 grid-header">Select</div>
         </div>
-        {questonBank.map((quesion, idx) => {
+        {questonBank.map((quesion) => {
           return (
             <div className="row">
               <div className="col-md-4">{quesion.questionText}</div>
@@ -19,12 +75,82 @@ export const QuestionBankDashboard = () => {
               <div className="col-md-3">{quesion.exams.join(", ")}</div>
               <div className="col-md-1">{quesion.difficulty}</div>
               <div className="col-md-1">
-                <checkbox id={idx}></checkbox>
+                <input
+                  type="checkbox"
+                  onChange={() => onQuestionSelect(quesion)}
+                />
               </div>
             </div>
           );
         })}
       </div>
+      {selectedQuestions.length > 0 && (
+        <>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4 grid-header">Question</div>
+              <div className="col-md-2 grid-header">Question Number</div>
+            </div>
+            {selectedQuestions.map((selectedQuestion, idx) => {
+              return (
+                <div className="row width-500">
+                  <div className="col-md-4">
+                    {
+                      questonBank.find(
+                        (ques) =>
+                          ques.questionId === selectedQuestion.questionId
+                      )?.questionText
+                    }
+                  </div>
+                  <div className="col-md-1">
+                    {selectedQuestion.questionNumber}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div style={{ float: "right", margin: "0 30px" }}>
+            <button onClick={handleDownloadPdf}>
+              Download Sample Question Paper
+            </button>
+          </div>
+          <div className="sample-question-paper" ref={printRef}>
+            <div style={{ marginLeft: "300px", marginTop: "60px" }}>
+              <h3>Sample Question Paper</h3>
+            </div>
+            {selectedQuestions.map((selectedQuestion) => {
+              return (
+                <>
+                  <div className="container margin-y-20">
+                    <div className="row">
+                      <div className="col-md-12 margin-y-5">
+                        Question {`${selectedQuestion.questionNumber}. `}
+                        {selectedQuestion.questionText}
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6">
+                        a. {selectedQuestion.answers[0]}
+                      </div>
+                      <div className="col-md-6">
+                        b. {selectedQuestion.answers[1]}
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6">
+                        c. {selectedQuestion.answers[2]}
+                      </div>
+                      <div className="col-md-6">
+                        d. {selectedQuestion.answers[3]}
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </>
+      )}
     </>
   );
 };
--- src/components/mock-paper/questionBank.js
+++ src/components/mock-paper/questionBank.js
@@ -13,31 +13,42 @@ const EXAMS = {
 
 export const questonBank = [
   {
+    questionId: 0,
     questionText: "Zeroth question?",
     answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
     difficulty: DIFFICULTY.hard,
     exams: [EXAMS.upNEET, EXAMS.upPCS],
   },
   {
+    questionId: 1,
     questionText: "First question?",
     answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
     difficulty: DIFFICULTY.hard,
     exams: [EXAMS.upNEET, EXAMS.upPCS],
   },
   {
+    questionId: 2,
     questionText: "Second question?",
     answers: ["Answer5", "Answer8", "Answer3", "Answer4"],
     difficulty: DIFFICULTY.hard,
     exams: [EXAMS.upNEET, EXAMS.upPCS],
   },
   {
+    questionId: 3,
     questionText: "Third question?",
-    answers: ["Answer1", "Answer44", "Answer4", "Answer6"],
+    answers: [
+      "Answer1",
+      "Answer44",
+      "Very very very Long Answer: sfkefo sfefml sfscs5353edcv svefgwef cscscsfegvs fegvdrdbvdvdsfbsdfvsdfbrehbsfvds dgvdvdvdvsdvsgegvdvedgedvdgegdv dvevedcdcegedvf fvbe",
+      "Answer6",
+    ],
     difficulty: DIFFICULTY.hard,
     exams: [EXAMS.upNEET, EXAMS.upPCS],
   },
   {
-    questionText: "Very Very long text in this question?",
+    questionId: 4,
+    questionText:
+      "Very Very long text in this question: dvbfrhhhh rgrhjtrbfd rwhernhtehj35 fhth45jhet  trht46jt her br r g r3th trgrhjtrbfd rwhernhtehj35 fhth45jhet  trht46jt her br r g r3th t  rgrhjtrbfd rwhernhtehj35 fhth45jhet  trht46jt her br r g r3th t rgrhjtrbfd rwhernhtehj35 fhth45jhet  trht46jt her br r g r3th t svsdrgrhjtrbfd rwhernhtehj35 fhth45jhet  trht46jt her br r g r3th t4nberbntejtyn er htejtntentebtentrnnenf fentrngten?",
     answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
     difficulty: DIFFICULTY.hard,
     exams: [EXAMS.upNEET, EXAMS.upPCS],
